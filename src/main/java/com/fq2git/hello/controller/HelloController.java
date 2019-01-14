f1f66c690c5faa13e29114b49d6525867505e85b
fq <1135918220@qq.com>
2019-01-14 10:19:04 +0800

    commit-by-idea

--- src/main/java/com/fq2git/hello/controller/HelloController.java
+++ src/main/java/com/fq2git/hello/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String HelloSpringBoot(){
-        return "Hello SpringBoot!!";
+        return "Hello SpringBoot!";
     }
 }
